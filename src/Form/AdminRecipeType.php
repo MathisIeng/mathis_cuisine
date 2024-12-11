81989975fd24ba9f73e625462c0b549f67bdfc7d
Tismma <mathis.ieng@lapiscine.pro>
2024-12-11 11:55:37 +0100

    add form

--- src/Form/AdminRecipeType.php
+++ src/Form/AdminRecipeType.php
@@ -7,6 +7,7 @@ use App\Entity\Recipe;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,14 +23,18 @@ class AdminRecipeType extends AbstractType
                 'label_attr' => ['class' => 'form_label'],
             ])
             ->add('image')
-            ->add('instructions')
+            ->add('instructions', TextareaType::class, [
+                'label' => 'Instructions',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form_label'],
+            ])
             ->add('ingredients')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'title',
             ])
             ->add('submit', SubmitType::class, [
-
+                'attr' => ['class' => 'btn btn-primary'],
             ])
         ;
     }
